41984ce47a64ebcea1a4ae8ae7199cb5f471dd0e
Pablo David Golzman <pablorapp@gmail.com>
2021-11-10 13:41:17 -0300

    inicio curso react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react"
+class Button extends Component {
+    constructor(props){
+        super(props)
+        console.log('constructor', this.props);
+    }
+    componentDidMount(){
+        console.log('componentDidMount');
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    componentDidUpdate(prevProps, prevState){
+        console.log('component Did Update', prevProps, prevState );
+    }
+
+    componentWillUnmount(){
+        console.log('desmontando componente');
+    }
+    render(){
+        console.log('ejecutando render');
+        return (
+            <button onClick={()=>this.setState({prop:1})}>Enviar</button>
+        )
+    }
 }
 
-export default App;
+class App extends Component {
+    state = {}
+    render(){
+        console.log(this.state);
+        return(
+            <div>
+                <p>Hola Mundo</p>
+                {this.state===3?<Button/>: null}
+                <button onClick={()=>this.setState({valor:2})}>Enviar en Ap</button>
+            </div>
+        )
+    }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -10,6 +10,26 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+// const X = () => <p>Hola mundo</p>
+// ReactDOM.render(
+//   <X/>,document.getElementById('root')
+// )
+// const Li = ({children, estado}) => {
+//   <li>{children} estoy {estado}</li>
+// }
+
+// const X = () => 
+// <ul>
+//   <Li 
+//   estado={'feliz'}
+//   casa={'false'}
+//   edad={24}>Chanco</Li>
+//   <Li estado={'triste'}></Li>
+//   <Li estado={'emosionado'}></Li>
+// </ul>
+// ReactDOM.render(
+//   <X/>,document.getElementById('root')
+// )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
